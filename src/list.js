22abe2305a7b8a2610379ea7735f38d389a08f37
tishayaem <kostiantynkalinin@gmail.com>
2018-07-19 11:40:06 +0100

    create-react-app setup; created list of teams

---
+++ src/list.js
@@ -0,0 +1,47 @@
+
+
+export const const listOfTeams = [
+
+    {
+        country: "England",
+        people: ["Aaron", "Zoltan"]
+    },
+
+    {
+        country: "China & Taiwan",
+        people: ["Justin", "Albert"]
+    },
+
+    {
+        country: "Spain",
+        people: ["Chris", "Ricardo"]
+    },
+
+    {
+        country: "Ukraine",
+        people: ["Constantin", "Cristian"]
+
+    },
+
+    {
+        country: "Wales",
+        people: ["Dave"]
+    },
+
+    {
+        country: "Brazil",
+        people: ["Damany", "Raj"]
+
+    },
+
+    {
+        country: "Slovenia",
+        people: ["Vesna"]
+
+    },
+
+    {
+        country: "Italy",
+        people: ["Vicky"]
+    }
+]
